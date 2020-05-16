da13f0fb898d20c895a61ade87b1fd9f28424622
vincent <1322499061@qq.com>
2020-05-17 00:00:55 +0800

    布局编写&初始化指示器

---
+++ music_voice/src/main/java/com/vincent/music_voice/view/home/HomeActivity.java
@@ -0,0 +1,115 @@
+package com.vincent.music_voice.view.home;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.vincent.music_voice.R;
+import com.vincent.music_voice.model.CHANNEL;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
+
+import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
+
+/**
+ * 首页Activity
+ */
+public class HomeActivity extends FragmentActivity implements View.OnClickListener {
+
+    private static final CHANNEL[] CHANNELS =
+            new CHANNEL[]{CHANNEL.MY, CHANNEL.DISCORY, CHANNEL.FRIEND};
+
+    /*
+     * View
+     */
+    private DrawerLayout mDrawerLayout;
+    private View mToggleView;
+    private View mSearchView;
+    private ViewPager mViewPager;
+    //private HomePagerAdapter mAdapter;
+    private View mDrawerQrcodeView;
+    private View mDrawerShareView;
+    private View unLogginLayout;
+    private ImageView mPhotoView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToggleView = findViewById(R.id.toggle_view);
+        mToggleView.setOnClickListener(this);
+        mSearchView = findViewById(R.id.search_view);
+        mSearchView.setOnClickListener(this);
+
+        mViewPager = findViewById(R.id.view_pager);
+
+        initMagicIndicator();
+    }
+
+    // 初始化指示器
+    private void initMagicIndicator() {
+        MagicIndicator magicIndicator = findViewById(R.id.magic_indicator);
+        magicIndicator.setBackgroundColor(Color.WHITE);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdjustMode(true);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+
+            @Override
+            public int getCount() {
+                return CHANNELS == null ? 0 : CHANNELS.length;
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                SimplePagerTitleView simplePagerTitleView = new ColorTransitionPagerTitleView(context);
+                simplePagerTitleView.setText(CHANNELS[index].getKey());
+                simplePagerTitleView.setTextSize(19);
+                simplePagerTitleView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+                simplePagerTitleView.setNormalColor(Color.parseColor("#999999"));
+                simplePagerTitleView.setSelectedColor(Color.parseColor("#333333"));
+                simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mViewPager.setCurrentItem(index);
+                    }
+                });
+                return simplePagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                return null;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
+
+    private void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}

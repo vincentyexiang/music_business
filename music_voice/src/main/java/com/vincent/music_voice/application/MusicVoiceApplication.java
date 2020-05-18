b0f850c5ef0908f51419505fbd2b7aad2c596bd8
vincent <1322499061@qq.com>
2020-05-19 07:11:41 +0800

    调试可运行状态

---
+++ music_voice/src/main/java/com/vincent/music_voice/application/MusicVoiceApplication.java
@@ -0,0 +1,29 @@
+package com.vincent.music_voice.application;
+
+import android.app.Application;
+
+
+public class MusicVoiceApplication extends Application {
+
+    private static MusicVoiceApplication mApplication = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mApplication = this;
+        //视频SDK初始化
+        //VideoHelper.init(this);
+        //音频SDK初始化
+        //AudioHelper.init(this);
+        //分享SDK初始化
+        //ShareManager.initSDK(this);
+        //更新组件下载
+        //UpdateHelper.init(this);
+        //ARouter初始化
+        //ARouter.init(this);
+    }
+
+    public static MusicVoiceApplication getInstance() {
+        return mApplication;
+    }
+}
